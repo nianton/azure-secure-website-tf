76bf01680c048456f29f40ac26cb61c49e5ebb5f
Nikolaos Antoniou <nianton@gmail.com>
2021-06-08 11:05:32 +0300

    Added bastion, revised subnets, sql auditing refactoring

--- deploy/vnet/main.tf
+++ deploy/vnet/main.tf
@@ -1,3 +1,47 @@
+resource "azurerm_virtual_network" "vnet" {
+  name                = var.name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  address_space       = ["10.0.0.0/16"]
+  dns_servers         = ["10.0.0.4", "10.0.0.5"]
+  tags                = var.tags
+}
+
+resource "azurerm_subnet" "appsubnet" {
+  name                                           = "snet-app-${var.name}"
+  resource_group_name                            = var.resource_group_name
+  virtual_network_name                           = azurerm_virtual_network.vnet.name
+  address_prefixes                               = ["10.0.1.0/24"]
+  enforce_private_link_endpoint_network_policies = true
+}
+
+resource "azurerm_subnet" "integrationsubnet" {
+  name                 = "snet-integration-${var.name}"
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+  delegation {
+    name = "delegation"
+    service_delegation {
+      name = "Microsoft.Web/serverFarms"
+    }
+  }
+}
+
+resource "azurerm_subnet" "adminsubnet" {
+  name                 = "snet-admin-${var.name}"
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.3.0/24"]
+}
+
+resource "azurerm_subnet" "bastionsubnet" {
+  name                 = "AzureBastionSubnet"
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.4.0/24"]
+}
+
 resource "azurerm_network_security_group" "nsg" {
   name                = "nsg-${var.name}"
   location            = var.location
@@ -5,55 +49,7 @@ resource "azurerm_network_security_group" "nsg" {
   tags                = var.tags
 }
 
-resource "azurerm_virtual_network" "vnet" {
-  name                = var.name
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  address_space       = ["10.0.0.0/16"]
-  dns_servers         = ["10.0.0.4", "10.0.0.5"]
-
-  subnet {
-    name           = "snet-app-${var.name}"
-    address_prefix = "10.0.1.0/24"
-  }
-
-  subnet {
-    name           = "snet-integration-${var.name}"
-    address_prefix = "10.0.2.0/24"
-  }
-
-  subnet {
-    name           = "snet-admin-${var.name}"
-    address_prefix = "10.0.3.0/24"
-    security_group = azurerm_network_security_group.nsg.id
-  }
-
-  subnet {
-    name           = "AzureBastionSubnet"
-    address_prefix = "10.0.4.0/24"
-  }
-
-  tags = var.tags
-}
-
-resource "azurerm_public_ip" "bastion_pip" {
-  name                = "pip-bastion"
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  allocation_method   = "Static"
-  sku                 = "Standard"
-  tags                = var.tags
-}
-
-resource "azurerm_bastion_host" "bastion" {
-  name                = "bastion-${var.name}"
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  tags                = var.tags
-
-  ip_configuration {
-    name                 = "configuration"
-    subnet_id            = element(azurerm_virtual_network.vnet.subnet[*].id, 3)
-    public_ip_address_id = azurerm_public_ip.bastion_pip.id
-  }
+resource "azurerm_subnet_network_security_group_association" "adminsubnetnsg" {
+  subnet_id                 = azurerm_subnet.adminsubnet.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
 }
